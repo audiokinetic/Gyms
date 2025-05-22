54a4db84417c8e2d55be9557180c4c9c5c76b9a4
Jean Gravel <jgravel@audiokinetic.com>
2025-05-22 15:48:57 -0400

    WG-78273 - Add EndOfEvent Callback Gym (Unity)
    Change-Id: I283accf904b69d2cc5855fdedc341eb799022926

--- Unity/Assets/Gyms/3-Advanced/AdvancedCallbackTypes/AdvancedCallbackTypesEndOfEvent/AdvancedCallbackTypesEndOfEvent.cs
+++ Unity/Assets/Gyms/3-Advanced/AdvancedCallbackTypes/AdvancedCallbackTypesEndOfEvent/AdvancedCallbackTypesEndOfEvent.cs
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
 The content of this file includes portions of the AUDIOKINETIC Wwise Technology
 released in source code form as part of the SDK installer package.
 
@@ -26,32 +26,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AdvancedSubtitle_Callback : MonoBehaviour
+public class AdvancedCallbackTypesEndOfEvent : MonoBehaviour
 {
 	[SerializeField]
-	TextMesh m_SubtitleText;
+	TextMesh m_CallbackText;
 
-	// Array containing the subtitles.
-	protected static string[] ms_EnglishSubtitles = new string[]
-	{
-		"In this tutorial...",
-		"...we will look at creating...",
-		"...actor-mixers...",
-		"...and control buses.",
-		"We will also look at the...",
-		"...actor-mixer and master-mixer structures...",
-		"...and how to manage these structures efficiently.",
-		"END OF DEMO."
-	};
-	
-	public void MarkerCallback(AkEventCallbackMsg callbackInfo)
+	protected static string ms_EndOfEventString = "Event has reached its end";
+
+	public void EndOfEventCallback(AkEventCallbackMsg callbackInfo)
 	{
 		switch (callbackInfo.type)
 		{
-		case AkCallbackType.AK_Marker:
-			AkMarkerCallbackInfo MarkerCallbackInfo = callbackInfo.info as AkMarkerCallbackInfo;
-			m_SubtitleText.text = ms_EnglishSubtitles[MarkerCallbackInfo.uIdentifier];
+		case AkCallbackType.AK_EndOfEvent:
+			m_CallbackText.text = ms_EndOfEventString;
 			break;
 		}
 	}
+
 }
