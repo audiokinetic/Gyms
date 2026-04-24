b2ef8f4b68d2b19533f726668782e35fcaf3441b
Victor Dion-Cormier <vdcormier@audiokinetic.com>
2026-04-24 15:21:31 -0400

    WG-79732: Update setup files
    Unreal Gyms have their binaries built
    Prompt that asks if you use Unreal or Unity
    
    Change-Id: I50e425d7d7bc80f57bff0ee18338d5508da1fb4c

--- setup_Windows.bat
+++ setup_Windows.bat
@@ -1,13 +1,46 @@
 @echo off
 set SCRIPT_DIR=%~dp0
 
+:start
 if "%~1"=="" (
-    set /p unityPath=Enter your Unity installation path:
+    set /p engine=Use Unreal or Unity:
 ) else (
-    set unityPath=%1
+    set engine=%1
 )
-echo Running Unity in batch mode to resolve packages. This may take a moment.
-%unityPath% -batchmode -resolvePackages -projectPath "Unity" -quit
+
+setlocal enabledelayedexpansion
+
+if /i "%engine%"=="Unity" (
+	if "%~2"=="" (
+        set /p unityPathComma="Enter your Unity Installation root path (e.g. C:/Program Files/Unity/Hub/Editor/Unity_6000.0.64f1):"
+	) else (
+	    set unityPathComma=%2
+	)
+	set "unityPath=!unityPathComma:"=!\Editor\Unity.exe"
+) else ( 
+	if /i "%engine%"=="Unreal" (
+		if "%~2"=="" (
+		    set /p unrealPathComma="Enter your Unreal Installation root path (e.g. C:/Program Files/Epic Games/UE_5.5):"
+        ) else (
+	        set unrealPathComma=%2
+	    )
+		set "unrealPath=!unrealPathComma:"=!"
+	) else (
+		echo Invalid Engine. Expected: Unreal or Unity
+		goto :start
+	)
+)
+
+if NOT "%unityPath%"=="" (
+	echo %unityPath%
+    echo Running Unity in batch mode to resolve packages. This may take a moment.
+    "%unityPath%" -batchmode -resolvePackages -projectPath "Unity" -quit
+)
+
+if NOT "%unrealPath%"=="" (
+    echo Running Unreal Build Tool. This may take a moment.
+	"%unrealPath%\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe" GymsEditor Win64 Development -project=%CD%\Unreal\Gyms.uproject
+)
 
 :: Generate the WAV files
 python WwiseProject/GenerateProjectWavFiles.py
@@ -18,30 +51,33 @@ python GenerateSupportedGyms.py
 :: Generate the SoundBanks
 "%WWISEROOT%\Authoring\x64\Release\bin\WwiseConsole.exe" generate-soundbank "%SCRIPT_DIR%WwiseProject/Gyms.wproj"
 
-:: Set the language for the Unity Project
-python Unity/Build/SetStartupLanguage.py
+if NOT "%unityPath%"=="" (
+    :: Set the language for the Unity Project
+    python Unity/Build/SetStartupLanguage.py
 
-start "" %unityPath% -batchmode -projectPath "Unity" -executeMethod AddressableInstaller.AddressableSetup
+    start "" "%unityPath%" -batchmode -projectPath "Unity" -executeMethod AddressableInstaller.AddressableSetup
 
-:: wait until unity is done setting up addressables
-set "processName=Unity.exe"
-set "isRunning=false"
-:checkProcess
-tasklist /fi "ImageName eq %processName%" /fo csv 2> NUL | find /I "%processName%" > NUL
-if "%ERRORLEVEL%"=="0" (
-    if not "%isRunning%"=="true" (
-        echo Setting up Unity Addressables
-        set "isRunning=true"
-    )
+    :: wait until unity is done setting up addressables
+    set "processName=Unity.exe"
+    set "isRunning=false"
+    :checkProcess
+    tasklist /fi "ImageName eq !processName!" /fo csv 2> NUL | find /I "!processName!" > NUL
+    if "%ERRORLEVEL%"=="0" (
+        if not "!isRunning!"=="true" (
+            echo Setting up Unity Addressables
+            set "isRunning=true"
+        )
 
-    timeout /t 1 /nobreak > nul
-    goto checkProcess
-) else (
-    if "%isRunning%"=="true" (
-        echo Addressables setup was completed successfully.
-
+        timeout /t 1 /nobreak > nul
+        goto checkProcess
     ) else (
-        echo An error occurred and prevented %processName% from running.
+        if "!isRunning!"=="true" (
+            echo Addressables setup was completed successfully.
+
+        ) else (
+            echo An error occurred and prevented !processName! from running.
+        )
     )
 )
+:commonexit
 pause
