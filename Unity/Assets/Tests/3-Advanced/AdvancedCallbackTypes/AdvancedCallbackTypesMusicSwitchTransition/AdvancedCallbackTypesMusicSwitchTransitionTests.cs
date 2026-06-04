652cb115cc0c01f3b1a85b1212002528599c12df
Isaac Gollish <igollish@audiokinetic.com>
2026-06-04 16:04:28 -0400

    WG-84251: Add MusicSwitchTransition callback Gym
    Change-Id: I52f6b9cf1b65fcf4be39b835c95d538151dcc34a

---
+++ Unity/Assets/Tests/3-Advanced/AdvancedCallbackTypes/AdvancedCallbackTypesMusicSwitchTransition/AdvancedCallbackTypesMusicSwitchTransitionTests.cs
@@ -0,0 +1,61 @@
+/*******************************************************************************
+The content of this file includes portions of the AUDIOKINETIC Wwise Technology
+released in source code form as part of the SDK installer package.
+
+Commercial License Usage
+
+Licensees holding valid commercial licenses to the AUDIOKINETIC Wwise Technology
+may use this file in accordance with the end user license agreement provided 
+with the software or, alternatively, in accordance with the terms contained in a
+written agreement between you and Audiokinetic Inc.
+
+Apache License Usage
+
+Alternatively, this file may be used under the Apache License, Version 2.0 (the 
+"Apache License"); you may not use this file except in compliance with the 
+Apache License. You may obtain a copy of the Apache License at 
+http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the Apache License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+OR CONDITIONS OF ANY KIND, either express or implied. See the Apache License for
+the specific language governing permissions and limitations under the License.
+*******************************************************************************/
+
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class AdvancedCallbackTypesMusicSwitchTransitionTests : GymTests
+    {
+        const string SceneName = "AdvancedCallbackTypesMusicSwitchTransition";
+        [UnityTest]
+        public IEnumerator AdvancedCallbackTypesMusicSwitchTransition_Tests()
+        {
+            yield return StartTest(SceneName);
+
+            AkEvent akEvent = gameObject.GetComponent<AkEvent>();
+
+#if AK_WWISE_ADDRESSABLES && UNITY_ADDRESSABLES
+#if UNITY_WEBGL
+			yield return akEvent.data.WwiseObjectReference.CompleteLoadBank();
+#else
+			yield return new WaitUntil(() => akEvent.data.WwiseObjectReference.CompleteLoadBank().IsCompleted);
+#endif
+#endif
+
+            akEvent.HandleEvent(gameObject);
+            yield return new WaitForSeconds(0.5f);
+            const int expected = 85;
+            var callbackInfo = gameObject.GetComponent<AdvancedCallbackTypesMusicSwitchTransitionTests_Callback>();
+            yield return new WaitUntil(() => (callbackInfo.BPM == expected), 
+                System.TimeSpan.FromSeconds(0.5f), () => {});
+            Assert.AreEqual(expected, callbackInfo.BPM);
+
+            yield return FinishTest(SceneName);
+        }
+    }
+}
